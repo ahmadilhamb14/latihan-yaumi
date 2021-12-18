e1093aeb24f5dc6778ffc4dfda7f9a638eb610f7
ahmadilhamb14 <56315963+ahmadilhamb14@users.noreply.github.com>
2021-12-18 14:18:48 +0800

    initial commit

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <link rel="stylesheet" href="Assets/css/style.css"> -->
     <link rel="stylesheet" href="style.css">
-    <title>Index</title>
+    <title>Ell punya</title>
 </head>
 
 <body>
